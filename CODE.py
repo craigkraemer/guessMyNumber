58962b26c017f9dd80ec80af15d9c76a3e566473
CRAIG KRAEMER <craig.kraemer@protonmail.com>
2025-08-15 11:54:51 -0400

    CODE.py

--- CODE.py
+++ CODE.py
@@ -5,7 +5,7 @@ flag = True
 guess = 0
 
 print("Guess my number 1-33: ", end= " ")
-while flag :                 # while flag remains True.
+while flag :                 # while continues to loop as long as flag remains True.
     
     guess = input()
     if not guess.isdigit() :
